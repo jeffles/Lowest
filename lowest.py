dc6bc00c765ab1f9bb75204818ace5a640a37e69
Jeff Sumner <jeff.sumner@gmail.com>
2022-09-23 13:47:44 -0500

    Added command line guesses and random entries and clearing the board

--- lowest.py
+++ lowest.py
@@ -12,6 +12,9 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from pprint import pprint
 
+# TODO - Current winner display
+# TODO - ELiminate all higher guesses when new lowest unique number is revealed
+
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/presentations']
 
@@ -118,13 +121,13 @@ def insert_cell_json(row, col, text):
                            "insertionIndex": 0}}
 
 def determine_color(num_picks):
-    return 1, 1, 1
-    # if num_picks > 1:
-    #     return 1, 0.1, 0.1
-    # elif num_picks == 0:
-    #     return .4, .4, .4
-    # else:
-    #     return 1, 1, 0
+    # return 1, 1, 1
+    if num_picks > 1:
+        return 1, 0.1, 0.1
+    elif num_picks == 0:
+        return .4, .4, .4
+    else:
+        return 1, 1, 0
 
 
 def update_color_json(row, col, color):
@@ -156,24 +159,24 @@ def update_color_json(row, col, color):
             }
         }
 
-def get_who_string(who_picked):
+def get_who_string(who_picked, max_length):
     who_str = ''
     if len(who_picked) == 0:
         return 'Noone'
 
     who_str = ', '.join(who_picked)
-    if len(who_str) > 30:
+    if len(who_str) > max_length:
         who_str = ''
         for person in who_picked:
             who_str += person.split()[0] + ', '
         who_str = who_str[:-2]
-    if len(who_str) > 30:
+    if len(who_str) > max_length:
         who_str = ''
         for person in who_picked:
             who_str += person.split()[0][0] + person.split()[1][0] + ','
         who_str = who_str[:-1]
-    if len(who_str) > 30:
-        who_str = who_str[:27] + '...'
+    if len(who_str) > max_length:
+        who_str = who_str[:max_length-3] + '...'
     return who_str
 
 
@@ -189,16 +192,75 @@ def get_remaining(participant_guesses, remaining_guesses):
         remaining[left].append(participant)
 
     remaining_strings = defaultdict(str)
-    remaining_strings[0] = get_who_string(remaining[0])
-    remaining_strings[1] = get_who_string(remaining[1])
-    remaining_strings[2] = get_who_string(remaining[2])
-    remaining_strings[3] = get_who_string(remaining[3])
-    remaining_strings[4] = get_who_string(remaining[4])
-    remaining_strings[5] = get_who_string(remaining[5])
+    remaining_strings[0] = get_who_string(remaining[0], 90)
+    remaining_strings[1] = get_who_string(remaining[1], 90)
+    remaining_strings[2] = get_who_string(remaining[2], 90)
+    remaining_strings[3] = get_who_string(remaining[3], 90)
+    remaining_strings[4] = get_who_string(remaining[4], 90)
+    remaining_strings[5] = get_who_string(remaining[5], 90)
     return remaining_strings
 
+def eliminate_squares(bigger, guesses):
+    requests = []
+    if len(bigger) == 0:
+        return
+    for pick in bigger:
+        col_index = (pick % 10) + 1
+        row_index = math.floor(pick / 10) + 2
+        num_picks = len(guesses[pick])
 
-def set_square(pick, num_picks, who_picked, remaining_participant_strings):
+        requests.append(delete_cell_json(row_index, col_index))
+        requests.append(insert_cell_json(row_index, col_index, str(num_picks)))
+        requests.append(update_color_json(row_index, col_index, determine_color(num_picks)))
+
+    body = {
+        'requests': requests
+    }
+
+    response = OATH_SERVICE.presentations().batchUpdate(
+        presentationId=PRESENTATION_ID, body=body).execute()
+
+
+def reset_slides(participants):
+    participants_str = get_who_string(participants, 90)
+
+    requests = [
+        delete_cell_json(0, 13),
+        insert_cell_json(0, 13, '-'),
+        # who picked
+        delete_cell_json(1, 13),
+        insert_cell_json(1, 13, 'Noone'),
+        # Remaining guesses
+        delete_cell_json(2, 13),
+        insert_cell_json(2, 13, participants_str),
+        delete_cell_json(4, 13),
+        insert_cell_json(4, 13, 'Noone'),
+        delete_cell_json(6, 13),
+        insert_cell_json(6, 13, 'Noone'),
+        delete_cell_json(8, 13),
+        insert_cell_json(8, 13, 'Noone'),
+        delete_cell_json(10, 13),
+        insert_cell_json(10, 13, 'Noone'),
+        delete_cell_json(12, 13),
+        insert_cell_json(12, 13, 'Noone'),
+        # Winner
+        delete_cell_json(0, 0),
+        insert_cell_json(0, 0, 'WELCOME TO LOWEST UNIQUE NUMBER!'),
+    ]
+    for col_index in range(1, 11):
+        for row_index in range(2, 14):
+            requests.append(delete_cell_json(row_index, col_index))
+            requests.append(insert_cell_json(row_index, col_index, '?'))
+            requests.append(update_color_json(row_index, col_index, (1, 1, 1)))
+    requests.append(update_color_json(2, 1, (0, 0, 0)))
+    body = {
+        'requests': requests
+    }
+    response = OATH_SERVICE.presentations().batchUpdate(
+        presentationId=PRESENTATION_ID, body=body).execute()
+
+
+def set_square(pick, num_picks, who_picked, remaining_participant_strings, winner):
     col_index = (pick % 10) + 1
     row_index = math.floor(pick / 10) + 2
 
@@ -212,25 +274,27 @@ def set_square(pick, num_picks, who_picked, remaining_participant_strings):
         insert_cell_json(0, 13, 'Current Guess: ' + str(pick)),
         # who picked
         delete_cell_json(1, 13),
-        insert_cell_json(1, 13, get_who_string(who_picked)),
+        insert_cell_json(1, 13, get_who_string(who_picked, 30)),
         # Remaining guesses
+        delete_cell_json(2, 13),
+        insert_cell_json(2, 13, remaining_participant_strings[5]),
+        delete_cell_json(4, 13),
+        insert_cell_json(4, 13, remaining_participant_strings[4]),
         delete_cell_json(6, 13),
-        insert_cell_json(6, 13, remaining_participant_strings[5]),
-        delete_cell_json(7, 13),
-        insert_cell_json(7, 13, remaining_participant_strings[4]),
+        insert_cell_json(6, 13, remaining_participant_strings[3]),
         delete_cell_json(8, 13),
-        insert_cell_json(8, 13, remaining_participant_strings[3]),
-        delete_cell_json(9, 13),
-        insert_cell_json(9, 13, remaining_participant_strings[2]),
+        insert_cell_json(8, 13, remaining_participant_strings[2]),
         delete_cell_json(10, 13),
         insert_cell_json(10, 13, remaining_participant_strings[1]),
-        delete_cell_json(11, 13),
-        insert_cell_json(11, 13, remaining_participant_strings[0]),
+        delete_cell_json(12, 13),
+        insert_cell_json(12, 13, remaining_participant_strings[0]),
+        #Winner
+        delete_cell_json(0, 0),
+        insert_cell_json(0, 0, 'Current Winner: ' + winner),
     ]
     body = {
         'requests': requests
     }
-    time.sleep(1.1)
     response = OATH_SERVICE.presentations().batchUpdate(
         presentationId=PRESENTATION_ID, body=body).execute()
 
@@ -240,60 +304,87 @@ def main():
 
     guesses = defaultdict(list)
     participant_guesses = defaultdict(list)
-    top_range = 100
+    top_range = 89
+
 
     def set_guesses(participant, nums):
         for n in nums:
             guesses[n].append(participant)
             participant_guesses[participant].append(n)
 
-    set_guesses('Calvin Wang', [17, 19, 23, 29, 31])
-    set_guesses('David Altuve', [1, 9, 15, 21, 49])
-    set_guesses('Lauren Davis', [11, 13, 17, 53, 97])
-    set_guesses('Chris Reynolds', [1, 2, 3, 4, 5])
-    set_guesses('George Guo', [1, 2, 3, 4, 5])
-    set_guesses('Justin Elms', [7, 9, 11, 13, 15])
-    set_guesses('Keith Loy', [3, 6, 7, 8, 9])
-    set_guesses('Newell Rose', [16, 20, 25, 30, 36])
-    set_guesses('Omar Mujahid', [3, 9, 17, 22, 101])
-    set_guesses('Rob Battaglia', [5, 8, 11, 13, 14])
-    set_guesses('Shuaiyuan Zhou', [1, 7, 11, 29, 38])
-    set_guesses('Stephen Kattner', [1, 16, 17, 18, 19])
-    set_guesses('Amr Alaas', [11, 17, 23, 29, 31])
-    set_guesses('Brian Alexander', [1, 17, 23, 33, 43])
-    set_guesses('Natalie Rees', [13, 14, 21, 33])
-    # set_guesses('Stacy Chen', random.sample(range(1, top_range), 5))
-    set_guesses('Stacy Chen', [13, 68, 101, 237, 1229])
-    set_guesses('Tonaz Perez Valadez', [10, 15, 16, 17, 18])
-    set_guesses('Janie Clarke', [8, 13, 24, 89, 112])
-    set_guesses('Jeff Sumner', [10, 15, 20, 23, 25])
-    set_guesses('Jim Sigler', [1, 5, 7, 11, 13])
-    set_guesses('Tim Zenchenko', [7, 11, 17, 23, 24])
-    set_guesses('Andrew Vinas', [3, 132, 163, 179, 114])
-    set_guesses('Olivia Griffin', [4, 11, 13, 17, 27])
-    set_guesses('Sarah Johnson', [8, 14, 19, 22, 31])
-    set_guesses('Steven Boydston', [1, 3, 7, 8, 9])
-    set_guesses('Ignacio Martelli', [1, 3, 7, 12, 14])
-    set_guesses('Marcos Mezzabotta', [5, 14, 18, 29, 53])
-    set_guesses('Ruzana Nekhay', [11, 13, 17, 19, 23])
-    set_guesses('Maxwell Rose', [6, 12, 14, 18, 28])
+    participants = ['Calvin Wang', 'David Altuve', 'Lauren Davis', 'Chris Reynolds', 'George Guo', 'Justin Elms',
+                    'Keith Loy', 'Newell Rose', 'Omar Mujahid', 'Rob Battaglia', 'Shuaiyuan Zhou', 'Stephen Kattner',
+                    'Amr Alaas', 'Brian Alexander', 'Natalie Rees', 'Stacy Chen', 'Tonaz Perez Valadez', 'Janie Clarke',
+                    'Jeff Sumner', 'Jim Sigler', 'Tim Zenchenko', 'Andrew Vinas', 'Olivia Griffin', 'Sarah Johnson',
+                    'Steven Boydston']
+    for guest in participants:
+        set_guesses(guest, random.sample(range(1, top_range), 5))
+
+    # set_guesses('Calvin Wang', [17, 19, 23, 29, 31])
+    # set_guesses('David Altuve', [1, 9, 15, 21, 49])
+    # set_guesses('Lauren Davis', [11, 13, 17, 53, 97])
+    # set_guesses('Chris Reynolds', [1, 2, 3, 4, 5])
+    # set_guesses('George Guo', [1, 2, 3, 4, 5])
+    # set_guesses('Justin Elms', [7, 9, 11, 13, 15])
+    # set_guesses('Keith Loy', [3, 6, 7, 8, 9])
+    # set_guesses('Newell Rose', [16, 20, 25, 30, 36])
+    # set_guesses('Omar Mujahid', [3, 9, 17, 22, 101])
+    # set_guesses('Rob Battaglia', [5, 8, 11, 13, 14])
+    # set_guesses('Shuaiyuan Zhou', [1, 7, 11, 29, 38])
+    # set_guesses('Stephen Kattner', [1, 16, 17, 18, 19])
+    # set_guesses('Amr Alaas', [11, 17, 23, 29, 31])
+    # set_guesses('Brian Alexander', [1, 17, 23, 33, 43])
+    # set_guesses('Natalie Rees', [13, 14, 21, 33])
+    # # set_guesses('Stacy Chen', random.sample(range(1, top_range), 5))
+    # set_guesses('Stacy Chen', [13, 68, 101, 237, 1229])
+    # set_guesses('Tonaz Perez Valadez', [10, 15, 16, 17, 18])
+    # set_guesses('Janie Clarke', [8, 13, 24, 89, 112])
+    # set_guesses('Jeff Sumner', [10, 15, 20, 23, 25])
+    # set_guesses('Jim Sigler', [1, 5, 7, 11, 13])
+    # set_guesses('Tim Zenchenko', [7, 11, 17, 23, 24])
+    # set_guesses('Andrew Vinas', [3, 132, 163, 179, 114])
+    # set_guesses('Olivia Griffin', [4, 11, 13, 17, 27])
+    # set_guesses('Sarah Johnson', [8, 14, 19, 22, 31])
+    # set_guesses('Steven Boydston', [1, 3, 7, 8, 9])
+    # set_guesses('Ignacio Martelli', [1, 3, 7, 12, 14])
+    # set_guesses('Marcos Mezzabotta', [5, 14, 18, 29, 53])
+    # set_guesses('Ruzana Nekhay', [11, 13, 17, 19, 23])
+    # set_guesses('Maxwell Rose', [6, 12, 14, 18, 28])
 
     conflicts = defaultdict(int)
     for num in guesses:
         conflicts[len(guesses[num])] += 1
 
+    reset_slides(participants)
     remaining_guesses = list(range(1, 120))
-    print(remaining_guesses)
+    winner = 'Noone'
     while len(remaining_guesses) > 0:
-        pick = remaining_guesses.pop(random.randrange(len(remaining_guesses)))
+        print("Pick a number - enter for random pick>")
+        my_pick = input()
+        if my_pick == '':
+            pick = remaining_guesses.pop(random.randrange(len(remaining_guesses)))
+            print(f'I picked {pick} for you.')
+        else:
+            pick = int(my_pick)
+            if pick not in remaining_guesses:
+                print('Not a valid pick. Try again')
+            else:
+                remaining_guesses.remove(pick)
+
+        if len(guesses[pick]) == 1:
+            winner = guesses[pick][0] + " with guess of " + str(pick)
+            bigger = [x for x in remaining_guesses if x > pick]
+            remaining_guesses = [x for x in remaining_guesses if x < pick]
+            eliminate_squares(bigger, guesses)
+
         remaining_participants_strings = get_remaining(participant_guesses, remaining_guesses)
-        set_square(pick, len(guesses[pick]), guesses[pick], remaining_participants_strings)
+        set_square(pick, len(guesses[pick]), guesses[pick], remaining_participants_strings, winner)
     exit()
 
     print("Guesses")
     pprint(guesses)
 
-    winner = None
+
     winning_number = 1
     while not winner:
         print(f'Guessers for {winning_number}')
